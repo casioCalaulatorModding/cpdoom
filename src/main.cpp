3aeacdf4ef2b689215680766a6e6ff0807a65da7
luke-e-gibson <luke.e.gibson@outlook.com>
2024-02-06 17:08:06 +1100

    start doom port

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <sdk/os/lcd.hpp>
 #include <sdk/os/debug.hpp>
 
+
+#include <doomgeneric/doomgeneric.h>
+
 /*
  * Fill this section in with some information about your app.
  * All fields are optional - so if you don't need one, take it out.
@@ -14,51 +17,21 @@ APP_VERSION("0.0.1")
 
 
 
-extern "C"
-
-void main() {
+extern "C" void main(int argc, char** argv) {
 	calcInit(); //backup screen and init some variables
-
+	doomgeneric_Create(argc, argv);
 	// Put your app's code here!
-
-	//Example for fillScreen(color);
-	fillScreen(color(0,64,0));
-
-	//Example for Debug_Printf(x,y,invert_color,0,format_string) //(small text)
-	Debug_Printf(10,1,false,0,"HelloWorld%d",42);
-
-	//Example for Debug_PrintString(string, invert_color) //(big text)
-	Debug_SetCursorPosition(13,1);
-	Debug_PrintString("HelloWorld",0);
-
-	//use this command to actually update the screen 
-	LCD_Refresh();
-
-	//Example for setPixel(x,y,color)
-	for (int x=0; x<256;x++){
-		for (int y=0; y<256; y++){
-			setPixel(50+x,250+y, color(x,y,0) );
+	bool keepRunning = true;
+	while (keepRunning)
+	{
+		doomgeneric_Tick();
+		uint32_t key1, key2;
+		getKey(&key1, &key2);
+		if(testKey(key1, key2, KEY_BACKSPACE)){
+			keepRunning = false;
 		}
 	}
-
-	//Example for triangle(x0,y0,x1,y1,x2,y2,colorFill,colorLine);
-	triangle(10,20,40,250,300,100,color(0,255,0),color(0,0,255));
-
-	//Example for line(x1,y1,x2,y2,color);
-	line(100,30,290,500,color(255,0,0));      //Use RGB color
-	line(110,30,300,500,0b1111100000000000);  //Or use 565 color
-
-	//Don't forget to do LCD_Refresh after setPixel(); line(); and triangle();
-	LCD_Refresh();
-
-	//Example for getKey
-	while(true){
-		uint32_t key1, key2;	//First create variables
-		getKey(&key1, &key2);	//then read the keys
-		if(testKey(key1, key2, KEY_BACKSPACE)){ //Use testKey() to test if a specific key is pressed
-			break;
-		}
-	}
-
-	calcEnd(); //restore screen and do stuff
+	Debug_Printf(10, 1, false, 0, "Doom Cleanup%d", 42);
+	calcEnd();
+	
 }
